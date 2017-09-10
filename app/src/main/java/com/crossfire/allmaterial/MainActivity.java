28237c95b55e50c9492a5beab405dd07cd16dadd
Shaurya Jaiswal <Shaurya Jaiswal>
2017-09-11 01:52:37 +0530

    First Commit

--- app/src/main/java/com/crossfire/allmaterial/MainActivity.java
+++ app/src/main/java/com/crossfire/allmaterial/MainActivity.java
@@ -6,13 +6,10 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mButton= (Button) findViewById(R.id.button);
     }
 }
